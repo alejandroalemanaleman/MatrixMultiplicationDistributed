8d3fa1fd30cd6776e1e12e7cce0ce32fd2537678
alejandroalemanaleman <alemanaleman.alejandro@gmail.com>
2025-01-04 11:40:01 +0000

    Minor changes in DistributedMatrixMultiplication.java

--- src/main/java/org/example/DistributedMatrixMultiplication.java
+++ src/main/java/org/example/DistributedMatrixMultiplication.java
@@ -37,6 +37,7 @@ public class DistributedMatrixMultiplication {
                 for (int j = 0; j < cols; j++) {
                     for (int k = 0; k < size; k++) {
                         result[i][j] += chunkA[i][k] * matrixB[k][j];
+                        System.out.println(result[i][j]);
                     }
                 }
             }
